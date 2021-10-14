a1359b048120977a5d717be815c701ebcdf5ede9
MichalPiotrowski0786 <michal.piotrowski.g@gmail.com>
2021-10-14 19:44:28 +0200

    Commenting done

--- src/components/Wxmap.vue
+++ src/components/Wxmap.vue
@@ -28,7 +28,7 @@ export default {
       this.mapObject.setView([52.2324042317635, 21.008814032139522], 6); // set initial view to center of Warsaw, with default zoom as 6
 
       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.mapObject); // add map display to map object(without this the map is empty)
-      const debugArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+      const debugArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; // initialize simple debug array
       this.DebugAddStations(debugArr); // add debug random stations
     },
     removeLeafletMapFromMemory() {
@@ -38,24 +38,25 @@ export default {
     },
     DebugAddStations(arr) {
       if (arr && arr.length > 0) {
-        arr.forEach((e) => this.DebugStationFactory(e));
+        arr.forEach((e) => this.DebugStationFactory(e)); // for every entry in array call DebugStationFactory to add marker to map
       }
     },
     DebugStationFactory(value) {
-      let lat = Math.random();
-      lat = this.remap(lat, 0, 1, 48.5, 55.0);
-      let lon = Math.random();
-      lon = this.remap(lon, 0, 1, 14.0, 24.5);
+      let lat = Math.random(); // random 0-1 latitude
+      lat = this.remap(lat, 0, 1, 48.5, 55.0); // remap latitude from 0-1 to 48.5-55.5
+      let lon = Math.random(); // random 0-1 longtitude
+      lon = this.remap(lon, 0, 1, 14.0, 24.5);// remap longtitude from 0-1 to 14.0-24.5
 
-      const marker = new L.Marker([lat, lon], { opacity: 0.0 }); // opacity may be set to zero
-      marker.bindTooltip(`${value} °C`, {
-        permanent: true,
-        direction: 'center',
+      const marker = new L.Marker([lat, lon], { opacity: 0.0 }); // leaflet marker variable, opacity may be set to zero
+      marker.bindTooltip(`${value} °C`, { // add celcius value to marker's tooltip
+        permanent: true, // always display
+        direction: 'center', // center tooltip
       });
-      marker.addTo(this.mapObject);
+      marker.addTo(this.mapObject); // add marker object to map
     },
     remap(value, inmin, inmax, outmin, outmax) {
       return ((value - inmin) * (outmax - outmin)) / (inmax - inmin) + outmin;
+      // remap values from one range to other, very usefull. TODO: make this function global for future possible use!
     },
   },
 };
